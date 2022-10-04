bd3f49f2d8a8b4b90b722464e5f74e82e56a764a
chanwit-y <chanwit.yimneam@hotmail.com>
2022-10-04 17:58:08 +0700

    insert seed to db

--- backend/src/_tests/model_db.rs
+++ backend/src/_tests/model_db.rs
@@ -5,7 +5,7 @@ async fn model_db_init_db() -> Result<(), Box<dyn std::error::Error>> {
     let db = init_db().await?;
 
     // let result = sqlx::query("SELECT * from todo").fetch_all(&db).await?;
-    assert_eq!(2, 2, "number of seed todos");
+//     assert_eq!(2, 2, "number of seed todos");
 
     Ok(())
 }
--- backend/src/model/db.rs
+++ backend/src/model/db.rs
@@ -1,16 +1,57 @@
-use std::time::Duration;
-
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use std::{fs, path::PathBuf};
 
 const PG_HOST: &str = "localhost";
 const PG_DB: &str = "pg_demo";
 const PG_USER: &str = "postgres";
 const PG_PWD: &str = "postgres";
 
+const SQL_DIR: &str = "sql/";
+const SQL_RECREATE: &str = "sql/00-recreate-db.sql";
+
 pub type Db = Pool<Postgres>;
 
 pub async fn init_db() -> Result<Db, sqlx::Error> {
+    {
+        let root_db = new_db_pool(PG_HOST, PG_DB, PG_USER, PG_PWD, 1).await?;
+        pexec(&root_db, SQL_RECREATE).await?;
+    }
+    let app_db = new_db_pool(PG_HOST, PG_DB, PG_USER, PG_PWD, 1).await?;
+    let mut paths: Vec<PathBuf> = fs::read_dir(SQL_DIR)?
+        .into_iter()
+        .filter_map(|e| e.ok().map(|e| e.path()))
+        .collect();
+
+    paths.sort();
+
+    for path in paths {
+        if let Some(path) = path.to_str() {
+            if path.ends_with(".sql") && path != SQL_RECREATE {
+                pexec(&app_db, &path).await?;
+            }
+        }
+    }
+
     new_db_pool(PG_HOST, PG_DB, PG_USER, PG_PWD, 1).await
+}
+
+async fn pexec(db: &Db, file: &str) -> Result<(), sqlx::Error> {
+    // Read the file
+    let content = fs::read_to_string(file).map_err(|ex| {
+        println!("Error reading {} (cause: {:?})", file, ex);
+        ex
+    })?;
+
+    let sqls: Vec<&str> = content.split(";").collect();
+
+    for sql in sqls {
+        match sqlx::query(&sql).execute(db).await {
+            Ok(_) => (),
+            Err(ex) => println!("Warning - pexec - Sql file '{}' Failed cause: {}", file, ex),
+        }
+    }
+
+    Ok(())
 }
 
 async fn new_db_pool(
