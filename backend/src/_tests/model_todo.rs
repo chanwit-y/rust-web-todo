eafa9a8e8ec2aadd94ba6cf133071999303f7f07
chanwit-y <chanwit.yimneam@hotmail.com>
2022-10-07 09:41:40 +0700

    add create todo & unit test create todo

--- backend/src/_tests/model_todo.rs
+++ backend/src/_tests/model_todo.rs
@@ -1,16 +1,39 @@
 use crate::model::db::init_db;
 
-use super::TodoMac;
+use super::{TodoMac, TodoPatch};
 
+#[tokio::test]
+async fn model_todo_create() -> Result<(), Box<dyn std::error::Error>> {
+    let db = init_db().await?;
+    let data_fx = TodoPatch {
+        title: Some("test - model_todo_create 1".to_string()),
+        ..Default::default()
+    };
+
+    let todo_created = TodoMac::create(&db, data_fx.clone()).await?;
+    println!("\n\n->> {:?}", todo_created);
+    assert!(todo_created.id >= 1000, "Id should be >= 1000");
+    assert_eq!(data_fx.title.unwrap(), todo_created.title);
+
+    Ok(())
+}
 
 #[tokio::test]
 async fn model_todo_list() -> Result<(), Box<dyn std::error::Error>> {
+    let db = init_db().await?;
 
-	let db = init_db().await?;
+    let todos = TodoMac::list(&db).await?;
 
-	let todos = TodoMac::list(&db).await?;
+    assert!(todos.len() > 0, "todos len should be > 0");
+//     assert_eq!(2, todos.len());
+//     println!("\n\n->> {:?}", todos);
+    // todo 101
+//     assert_eq!(101, todos[0].id);
+//     assert_eq!(123, todos[0].cid);
+    // todo 100
+//     assert_eq!(100, todos[1].id);
+//     assert_eq!(123, todos[1].cid);
+//     assert_eq!("todo 100", todos[1].title);
 
-	assert_eq!(2, todos.len());
-
-	Ok({})
+    Ok({})
 }
--- backend/src/model/todo.rs
+++ backend/src/model/todo.rs
@@ -1,4 +1,5 @@
 use super::db::Db;
+use crate::model;
 
 // region: Todo Types
 #[derive(sqlx::FromRow, Debug, Clone)]
@@ -7,25 +8,40 @@ pub struct Todo {
     pub cid: i64,
     pub title: String,
 }
+
+#[derive(Default, Clone)]
+pub struct TodoPatch {
+    pub cid: Option<i64>,
+    pub title: Option<String>,
+}
 // endregion: Todo Types
 
 // region: TodoMac
 pub struct TodoMac;
 
 impl TodoMac {
-    pub async fn list(db: &Db) -> Result<Vec<Todo>, sqlx::Error> {
+    pub async fn create(db: &Db, data: TodoPatch) -> Result<Todo, model::Error> {
+        let sql = "insert into todo (cid, title) values ($1,$2) returning id, cid, title";
+        let query = sqlx::query_as::<_, Todo>(&sql)
+            .bind(123 as i64)
+            .bind(data.title.unwrap_or_else(|| "untitled".to_string()));
+        let todo = query.fetch_one(db).await?;
+
+        Ok(todo)
+    }
+
+    pub async fn list(db: &Db) -> Result<Vec<Todo>, model::Error> {
         let sql = "SELECT id, cid, title FROM todo ORDER BY id desc";
 
         // let query = sqlx::query_as::<_, Todo>(&sql);
         let query = sqlx::query_as(&sql);
         let todos = query.fetch_all(db).await?;
 
-	Ok(todos)
+        Ok(todos)
     }
 }
 // endregion:  TodoMac
 
-
 #[cfg(test)]
-#[path ="../_tests/model_todo.rs"]
+#[path = "../_tests/model_todo.rs"]
 mod tests;
