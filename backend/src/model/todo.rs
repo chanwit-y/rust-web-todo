3c7424afd597f6725461dd25549de1b392e98466
chanwit-y <chanwit.yimneam@hotmail.com>
2022-10-11 09:38:58 +0700

    change string query to sqlb

--- backend/src/model/todo.rs
+++ backend/src/model/todo.rs
@@ -1,3 +1,5 @@
+use sqlb::HasFields;
+
 use super::db::Db;
 use crate::model;
 
@@ -9,11 +11,21 @@ pub struct Todo {
     pub title: String,
 }
 
-#[derive(Default, Clone)]
+#[derive(sqlb::Fields, Default, Debug, Clone)]
 pub struct TodoPatch {
     pub cid: Option<i64>,
     pub title: Option<String>,
+    pub status: Option<TodoStatus>,
 }
+
+#[derive(sqlx::Type, Debug, Clone, PartialEq, Eq)]
+#[sqlx(type_name = "todo_status_enum")]
+#[sqlx(rename_all = "lowercase")]
+pub enum TodoStatus {
+    Open,
+    Clone,
+}
+sqlb::bindable!(TodoStatus);
 // endregion: Todo Types
 
 // region: TodoMac
@@ -21,21 +33,37 @@ pub struct TodoMac;
 
 impl TodoMac {
     pub async fn create(db: &Db, data: TodoPatch) -> Result<Todo, model::Error> {
-        let sql = "insert into todo (cid, title) values ($1,$2) returning id, cid, title";
-        let query = sqlx::query_as::<_, Todo>(&sql)
-            .bind(123 as i64)
-            .bind(data.title.unwrap_or_else(|| "untitled".to_string()));
-        let todo = query.fetch_one(db).await?;
+        // let sql = "insert into todo (cid, title) values ($1,$2) returning id, cid, title";
+        // let query = sqlx::query_as::<_, Todo>(&sql)
+        //     .bind(123 as i64) // Fixme - should come from user context
+        //     .bind(data.title.unwrap_or_else(|| "untitled".to_string()));
+        // let todo = query.fetch_one(db).await?;
+
+        let mut fields = data.fields();
+        fields.push(("cid", 124).into());
+
+        let sb = sqlb::insert()
+            .table("todo")
+            .data(data.fields())
+            .returning(&["id", "cid", "title", "status"]);
+
+        let todo = sb.fetch_one(db).await?;
 
         Ok(todo)
     }
 
     pub async fn list(db: &Db) -> Result<Vec<Todo>, model::Error> {
-        let sql = "SELECT id, cid, title FROM todo ORDER BY id desc";
+        // let sql = "SELECT id, cid, title FROM todo ORDER BY id desc";
 
-        // let query = sqlx::query_as::<_, Todo>(&sql);
-        let query = sqlx::query_as(&sql);
-        let todos = query.fetch_all(db).await?;
+        // let query = sqlx::query_as(&sql);
+        // let todos = query.fetch_all(db).await?;
+
+        let sb = sqlb::select()
+            .table("todo")
+            .columns(&["id", "cid", "title", "status"])
+            .order_by("!id");
+
+        let todos = sb.fetch_all(db).await?;
 
         Ok(todos)
     }
