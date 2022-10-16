d4c90c6e5481a2fd341da7edf9cfa8dc364e7b79
chanwit-y <chanwit.yimneam@hotmail.com>
2022-10-16 21:20:41 +0700

    add web

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,30 @@
+use std::{env, sync::Arc};
+
+use model::init_db;
+use web::start_web;
+
 
 mod model;
 mod security;
 mod web;
 
+const DEFAULT_WEB_FOLDER: &'static str = "web-folder/";
+const DEFAULT_WEB_PORT: u16 = 8080;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
-    
+    // compute the web_folder
+    let mut args: Vec<String> = env::args().collect();
+    let web_folder = args.pop().unwrap_or_else(|| DEFAULT_WEB_FOLDER.to_string());
+    let web_port = DEFAULT_WEB_PORT;
+
+    // get the database
+    let db = init_db().await.expect("Connot init db");
+    let db = Arc::new(db);
+
+    // start the server
+    match  start_web(&web_folder, web_port, db).await {
+       Ok(_) => println!("Server ended"),
+       Err(ex) => println!("Error - web server failed to start. Cause {:?}", ex) 
+    }
 }
--- backend/src/model/mod.rs
+++ backend/src/model/mod.rs
@@ -3,6 +3,10 @@ use thiserror::Error as ThisError;
 mod db;
 mod todo;
 
+// re-export
+pub use db::Db;
+pub use db::init_db;
+
 #[derive(ThisError, Debug)]
 pub enum Error {
 	#[error("Entity Not Found - {0}[{1}]")]
