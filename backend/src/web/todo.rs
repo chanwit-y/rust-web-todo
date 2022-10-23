cacdcf7f87c9ecd012c5847a14b5cecd2b69d4c7
chanwit-y <chanwit.yimneam@hotmail.com>
2022-10-23 16:27:40 +0700

    add web todo

--- backend/src/web/todo.rs
+++ backend/src/web/todo.rs
@@ -1,11 +1,19 @@
 use crate::model::Db;
-use std::sync::Arc;
+use std::{convert::Infallible, sync::Arc};
 use warp::Filter;
 
 pub fn todo_rest_filters(
     base_path: &'static str,
     db: Arc<Db>,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-
-	
 }
+
+// region: Filter Utils
+pub fn with_db(db: Arc<Db>) -> impl Filter<Extract = (Arc<Db>,), Error = Infallible> + Clone {
+    warp::any().map(move || db.clone())
+}
+
+pub fn do_auth(db: Arc<Db>) -> impl Filter<Extract = (UserCtx,), Error = Rejection> + Clone {
+    warp::any().and_then(|| async { Ok::<UseCtx, Rejection>(utx_from_token("123").await.unwrap()) })
+}
+// endregion: Filter Utils
